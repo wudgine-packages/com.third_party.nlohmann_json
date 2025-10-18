8181011bbd1b8527eca8a86fc60555acb29fa2f6
Wudafk <wudafk@gmail.com>
2025-10-18 16:37:26 +0700

    1

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "--- Processing nlohmann_json ---"
-JSON_NAME="nlohmann-json" # Уточнил имя, так как папка называется 'json'
+JSON_NAME="com.third_party.nlohmann_json.source@${PKG_VERSION}" # Имя с версией
 JSON_URL="https://github.com/nlohmann/json"
 JSON_TAG="v${PKG_VERSION}" # "v3.12.0" # Указываем нужный тег
 
@@ -23,21 +23,19 @@ DownloadDependency() {
 #  WUDGINE_ROOT=$(realpath "${SCRIPT_DIR}/../../.wudgine")
 #fi
 
-  DEPS_SOURCES_DIR="${WUDGINE_ROOT}/.deps"
-
   echo "Install ${DEP_NAME}..."
 
 
-  if [ -d "${DEPS_SOURCES_DIR}/${DEP_NAME}" ]; then
+  if [ -d "${THIRD_PARTY_DEPS_SOURCES_DIR}/${DEP_NAME}" ]; then
     echo "Dependency ${DEP_NAME} already was successfully installed."
     return;
   fi
 
-#  cd "${DEPS_SOURCES_DIR}"
-#  git clone "${DEP_GIT_URL}" "${DEPS_SOURCES_DIR}/${DEP_NAME}.git" || { echo "Error: Failed to install ${DEP_NAME}."; exit 1; }
+#  cd "${THIRD_PARTY_DEPS_SOURCES_DIR}"
+#  git clone "${DEP_GIT_URL}" "${THIRD_PARTY_DEPS_SOURCES_DIR}/${DEP_NAME}.git" || { echo "Error: Failed to install ${DEP_NAME}."; exit 1; }
 #  echo "Dependency ${DEP_NAME} install successfully."
 #  echo "Updating and pinning ${DEP_NAME} to tag ${DEP_GIT_TAG}..."
-#  cd "${DEPS_SOURCES_DIR}/${DEP_NAME}" || { echo "Error: Dependency directory for ${DEP_NAME} not found."; exit 1; }
+#  cd "${THIRD_PARTY_DEPS_SOURCES_DIR}/${DEP_NAME}" || { echo "Error: Dependency directory for ${DEP_NAME} not found."; exit 1; }
 #  git fetch origin
 #  git checkout "${DEP_GIT_TAG}" || { echo "Error: Failed to checkout tag ${DEP_GIT_TAG} for ${DEP_NAME}. Does it exist?"; exit 1; }
 #  echo "${DEP_NAME} pinned to tag ${DEP_GIT_TAG}."
@@ -45,16 +43,15 @@ DownloadDependency() {
 
 
 echo "PKG_VERSION = ${PKG_VERSION}"
-echo "DEPS_SOURCES_DIR = ${DEPS_SOURCES_DIR}"
-mkdir -p "${DEPS_SOURCES_DIR}"
+mkdir -p "${THIRD_PARTY_DEPS_SOURCES_DIR}"
 
-  cd "${DEPS_SOURCES_DIR}"
+  cd "${THIRD_PARTY_DEPS_SOURCES_DIR}"
   if [[ ! -d $$DEP_NAME ]]; then
 
-echo "git command = git clone --branch ${DEP_GIT_TAG} --depth 1 ${DEP_GIT_URL} ${DEPS_SOURCES_DIR}/${DEP_NAME}"
+echo "git command = git clone --branch ${DEP_GIT_TAG} --depth 1 ${DEP_GIT_URL} ${THIRD_PARTY_DEPS_SOURCES_DIR}/${DEP_NAME}"
 
   # Correct: Use git clone to get the repository as a folder.
-  git clone --branch "${DEP_GIT_TAG}" --depth 1 "${DEP_GIT_URL}" "${DEPS_SOURCES_DIR}/${DEP_NAME}" || {
+  git clone --branch "${DEP_GIT_TAG}" --depth 1 "${DEP_GIT_URL}" "${THIRD_PARTY_DEPS_SOURCES_DIR}/${DEP_NAME}" || {
       echo "Error: Failed to clone ${DEP_NAME}."
       exit 1
   }
